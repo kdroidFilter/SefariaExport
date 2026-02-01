7e82488271171d5ae4e094949d8a8c141aa60f3c
Elie Gambache <elyahou.hadass@gmail.com>
2026-02-01 15:56:31 +0200

    Fix variable name in print statement

--- configure_local_settings.py
+++ configure_local_settings.py
@@ -35,7 +35,7 @@ def main() -> None:
         f.write(s)
 
     print("✅ local_settings.py configured")
-    print(f"   SEFARIA_EXPORT_PATH = {export_base}")
+    print(f"   SEFARIA_EXPORT_PATH = {export_path}")
 
 
 if __name__ == "__main__":
